dc3ecb5eadc0e4555025f942284232e8099d8b2d
Alberto Jauregui <bastertwo@gmail.com>
2013-03-22 22:23:28 -0700

    Compress JS and CSS files

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,8 @@ $(function(){
 	var passwd = $("#inp-passwd").val();
 	var msg = "";
 	var errors = 0;
-	if(!domain){
-	  msg += "+ The domain is required<br />";
-	  errors++;
-	}
-	if(!passwd){
-	  msg += "+ The password is required<br />";
-	  errors++;
-	}
-	if(errors){
-	  $("#msg").html(msg).show();
-	  $("#inp-passcode").val("");
-	}else{
-	  $.get("generate.pl",{domain:domain, passwd:passwd},function(data){
-	      $("#inp-passcode").val(data);
-	    });
-	}
+	if(!domain){msg += "+ The domain is required<br />";errors++;}
+	if(!passwd){msg += "+ The password is required<br />";errors++;}
+	if(errors){$("#msg").html(msg).show();$("#inp-passcode").val("");}else{$.get("generate.pl",{domain:domain, passwd:passwd},function(data){$("#inp-passcode").val(data);});}
       });
   });
